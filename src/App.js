5dd5e78ac37316c5d1059cb017177302ed83bca5
lyndaruiz95 <lyndaruiz95@gmail.com>
2020-05-18 20:39:27 -0700

    initializing project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './assets/logo_white.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <body>
+        <button>
+              Create RetroBoard
+        </button>
+      </body>
+      
+    </div>
+  );
+}
+
+export default App;
